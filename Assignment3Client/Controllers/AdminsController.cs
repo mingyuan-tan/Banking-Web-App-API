d102eddd6282c878ba614378564aafa831f3f16d
Ming Yuan Tan <s3739706@student.rmit.edu.au>
2020-02-07 15:26:01 +1100

    reverting

--- Assignment3Client/Controllers/AdminsController.cs
+++ Assignment3Client/Controllers/AdminsController.cs
@@ -13,11 +13,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Assignment3Client.Controllers
 {
-
     [AuthorizeAdmin]
     public class AdminsController : Controller
     {
-        [Route("Home/Index")]
+
         public async Task<IActionResult> Index()
         {
             var response = await BankAPI.InitializeClient().GetAsync("api/Customers");
@@ -36,7 +35,6 @@ namespace Assignment3Client.Controllers
             return View(customers);
         }
 
-        [Route("Home/kokocrunch")]
         public async Task<IActionResult> EditCustomerProfile(int? id)
         {
             if (id == null)
@@ -86,7 +84,6 @@ namespace Assignment3Client.Controllers
             return View(customer);
         }
 
-        [Route("Home/milo")]
         public async Task<IActionResult> IndexToTransactions()
         {
             var response = await BankAPI.InitializeClient().GetAsync("api/Customers");
@@ -125,61 +122,8 @@ namespace Assignment3Client.Controllers
 
         }
 
-        public IActionResult Charts(int id, DateTime start, DateTime end)
-        {
-            var startFormatted = start.ToString("dd-MM-yyyy");
-            var endFormatted = end.ToString("dd-MM-yyyy");
 
-            ViewBag.id = id;
-            ViewBag.start = startFormatted;
-            ViewBag.end = endFormatted;
-
-            return View();
-        }
-
-        public async Task<IActionResult> CreateChartsData(int id, DateTime start, DateTime end)
-        {
-            //ViewBag.id = id;
-            //ViewBag.start = start;
-            //ViewBag.end = end;
-            
-            var startFormatted = start.ToString("dd-MM-yyyy");
-            var endFormatted = end.ToString("dd-MM-yyyy");
-
-            var response = await BankAPI.InitializeClient().GetAsync($"api/Customers/getCustomerTransactions/{id}/{startFormatted}/{endFormatted}");
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception();
-            }
-
-            // Storing response details received from the web api 
-            var result = response.Content.ReadAsStringAsync().Result;
-
-            // Deserializing the response received from web api and storing into a list 
-            var transactions = JsonConvert.DeserializeObject<List<Transaction>>(result);
-
-            var data = transactions.GroupBy(x => new { group = x.TransactionType })
-                                            .Select(group => new 
-                                            { 
-                                                transactionType = group.Key.group, count = group.Count() 
-                                            }).OrderByDescending(o => o.count).ToList();
-
-            var labels = data.Select(x => x.transactionType).ToArray();
-            var values = data.Select(x => x.count).ToArray();
-            var maxValue = values[0];
-
-            List<object> list = new List<object>();
-            list.Add(labels);
-            list.Add(values);
-            list.Add(maxValue);
-
-            return Json(list);
-        }
-
-
-        [Route("Home/cornflakes")]
-        public async Task<IActionResult> ViewBillPays(int id)
+       public async Task<IActionResult> ViewBillPays(int id)
         {
             var response = await BankAPI.InitializeClient().GetAsync($"api/Customers/getCustomerBillPays/{id}");
 
@@ -197,7 +141,6 @@ namespace Assignment3Client.Controllers
             return View(billPays);
         }
 
-        [Route("Home/uncletobys")]
         public async Task<IActionResult> EditBillPay(int? id)
         {
             if (id == null)
@@ -224,7 +167,6 @@ namespace Assignment3Client.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult EditBillPay(int id, BillPay billpay)
         {
-
             if (id != billpay.BillPayID)
             {
                 return NotFound();
@@ -232,17 +174,12 @@ namespace Assignment3Client.Controllers
 
             if (ModelState.IsValid)
             {
-
                 var content = new StringContent(JsonConvert.SerializeObject(billpay), Encoding.UTF8, "application/json");
                 var response = BankAPI.InitializeClient().PutAsync("api/BillPays", content).Result;
 
-                // These are to get customerID so it can be passed to ViewBillPays and get back into that page
-                var response2 = BankAPI.InitializeClient().GetAsync($"api/Customers/getCustomerFromBillPay/{id}");
-                int customerID = int.Parse(response2.Result.Content.ReadAsStringAsync().Result);
-
                 if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("ViewBillPays", new { id = customerID });
+                    return RedirectToAction("ViewBillPays");
                 }
             }
 
@@ -251,8 +188,6 @@ namespace Assignment3Client.Controllers
 
             return View(billpay);
         }
-
-    
 
     }
 }
